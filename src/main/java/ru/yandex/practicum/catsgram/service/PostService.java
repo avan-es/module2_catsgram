bfb1b17c20d480980f1b062640fc4683c8924b80
BagratPUH <avanesianbag@gmail.com>
2022-12-21 01:12:18 +0300

    Done Спринт 10. Тема 2. Урок 2. Параметры запроса. Модернизировать вывод постов с по, сортировка от нового к старому и обратно.

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -9,6 +9,7 @@ import ru.yandex.practicum.catsgram.model.Post;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
@@ -20,8 +21,14 @@ public class PostService {
         this.userService = userServiceIn;
     }
 
-    public List<Post> findAll() {
-        return posts;
+    public List<Post> findAll(String sort, Integer size, Integer from) {
+        return posts.stream().sorted((p0, p1) -> {
+            int comp = p0.getCreationDate().compareTo(p1.getCreationDate());
+            if (sort.equals("desc")) {
+                comp = -1 * comp;
+            }
+            return comp;
+        }).skip(from).limit(size).collect(Collectors.toList());
     }
 
     public Post create(Post post) {
